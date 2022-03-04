97a9ff8a7a630ad722b3d753e8911bfbedfbf840
Sam Krishna <sam.krishna@gmail.com>
2022-03-04 12:35:45 -0800

    Create Sign struct

--- Sources/SwissEphemeris/Sign.swift
+++ Sources/SwissEphemeris/Sign.swift
@@ -1,8 +1,23 @@
 //
-//  File.swift
+//  Sign.swift
 //  
 //
 //  Created by Sam Krishna on 3/4/22.
 //
 
 import Foundation
+
+/// Models the starting degree of a Zodiac sign
+public struct Sign {
+
+    /// The degree of the coordinate
+    public let value: Double
+
+    /// Creates a `Sign`.
+    /// - Parameter value: The latitudinal degree to set.
+    public init(value: Double) {
+        self.value = value
+    }
+}
+
+extension Sign: ZodiacCoordinate {}
