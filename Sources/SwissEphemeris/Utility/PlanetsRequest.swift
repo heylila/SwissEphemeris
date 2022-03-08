698a5ce69051c81ecb335479e198f91520350c94
Sam Krishna <sam.krishna@gmail.com>
2022-03-08 11:24:50 -0800

    Add drafts of 'LunarNodesRequest' and 'AsteroidsRequest' to PlanetRequests.swift

--- Sources/SwissEphemeris/Utility/PlanetsRequest.swift
+++ Sources/SwissEphemeris/Utility/PlanetsRequest.swift
@@ -27,3 +27,42 @@ final public class PlanetsRequest: BatchRequest {
         }
 	}
 }
+
+final public class AsteroidsRequest: BatchRequest {
+    /// The `Asteroids` to request.
+    private let body: Asteroid
+    public typealias EphemerisItem = Coordinate<Asteroid>
+    public let datesThreshold = 478
+
+    /// Creates an instance of `AsteroidsRequest`.
+    /// - Parameter body: The asteroid to request.
+    public init(body: Asteroid) {
+        self.body = body
+    }
+
+    public func fetch(start: Date, end: Date, interval: TimeInterval = 60.0) -> [EphemerisItem] {
+        stride(from: start, to: end, by: interval).map {
+            EphemerisItem(body: body, date: $0)
+        }
+    }
+
+}
+
+final public class LunarNodesRequest: BatchRequest {
+    /// The `LunarNode` to request.
+    private let body: LunarNode
+    public typealias EphemerisItem = Coordinate<LunarNode>
+    public let datesThreshold = 478
+
+    /// Creates an instance of `LunarNodesRequest`.
+    /// - Parameter body: The lunar node to request.
+    public init(body: LunarNode) {
+        self.body = body
+    }
+
+    public func fetch(start: Date, end: Date, interval: TimeInterval = 60.0) -> [EphemerisItem] {
+        stride(from: start, to: end, by: interval).map {
+            EphemerisItem(body: body, date: $0)
+        }
+    }
+}
