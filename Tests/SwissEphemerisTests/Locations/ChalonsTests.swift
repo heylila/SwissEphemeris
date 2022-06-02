ae568287c8f7dccc57c97082aa1efccc0e273817
Sam Krishna <sam.krishna@gmail.com>
2022-06-02 15:08:09 -0700

    Add house tests to Chalons-en-Champagne test case

--- Tests/SwissEphemerisTests/Locations/ChalonsTests.swift
+++ Tests/SwissEphemerisTests/Locations/ChalonsTests.swift
@@ -13,7 +13,7 @@ class ChalonsTests: XCTestCase {
     // Châlons-en-Champagne, France
     // Lat: 48.956682
     // Lng: 4.363073
-    // Date: 1908-05-05 09:30:00 +0010 (UTC)
+    // Date: 1908-05-05 09:20:39 +0000 (UTC)
 
     override func setUpWithError() throws {
         JPLFileManager.setEphemerisPath()
@@ -23,7 +23,7 @@ class ChalonsTests: XCTestCase {
         return Date(fromString: "1908-05-05 09:20:39 +0000", format: .cocoaDateTime, timeZone: .utc)!
     }
 
-    static var houseSystem: HouseCusps {
+    static var houseCusps: HouseCusps {
         let lat = 48.956682
         let long = 4.363073
         return HouseCusps(date: birthDate, latitude: lat, longitude: long, houseSystem: .placidus)
@@ -143,4 +143,99 @@ class ChalonsTests: XCTestCase {
         XCTAssert(6 == Int32(chiron.latitude))
     }
 
+    func testHouse1() throws {
+        let house = ChalonsTests.houseCusps.first
+        XCTAssert(house.sign == Zodiac.leo)
+        XCTAssert(0 == Int32(house.degree))
+        XCTAssert(5 == Int32(house.minute))
+        XCTAssert(44 == Int32(house.second))
+    }
+
+    func testHouse2() throws {
+        let house = ChalonsTests.houseCusps.second
+        XCTAssert(house.sign == Zodiac.leo)
+        XCTAssert(17 == Int32(house.degree))
+        XCTAssert(24 == Int32(house.minute))
+        XCTAssert(42 == Int32(house.second))
+    }
+
+    func testHouse3() throws {
+        let house = ChalonsTests.houseCusps.third
+        XCTAssert(house.sign == Zodiac.virgo)
+        XCTAssert(8 == Int32(house.degree))
+        XCTAssert(57 == Int32(house.minute))
+        XCTAssert(0 == Int32(house.second))
+    }
+
+    func testHouse4() throws {
+        let house = ChalonsTests.houseCusps.fourth
+        XCTAssert(house.sign == Zodiac.libra)
+        XCTAssert(8 == Int32(house.degree))
+        XCTAssert(1 == Int32(house.minute))
+        XCTAssert(32 == Int32(house.second))
+    }
+
+    func testHouse5() throws {
+        let house = ChalonsTests.houseCusps.fifth
+        XCTAssert(house.sign == Zodiac.scorpio)
+        XCTAssert(16 == Int32(house.degree))
+        XCTAssert(36 == Int32(house.minute))
+        XCTAssert(24 == Int32(house.second))
+    }
+
+    func testHouse6() throws {
+        let house = ChalonsTests.houseCusps.sixth
+        XCTAssert(house.sign == Zodiac.sagittarius)
+        XCTAssert(27 == Int32(house.degree))
+        XCTAssert(29 == Int32(house.minute))
+        XCTAssert(38 == Int32(house.second))
+    }
+
+    func testHouse7() throws {
+        let house = ChalonsTests.houseCusps.seventh
+        XCTAssert(house.sign == Zodiac.aquarius)
+        XCTAssert(0 == Int32(house.degree))
+        XCTAssert(5 == Int32(house.minute))
+        XCTAssert(44 == Int32(house.second))
+    }
+
+    func testHouse8() throws {
+        let house = ChalonsTests.houseCusps.eighth
+        XCTAssert(house.sign == Zodiac.aquarius)
+        XCTAssert(17 == Int32(house.degree))
+        XCTAssert(24 == Int32(house.minute))
+        XCTAssert(42 == Int32(house.second))
+    }
+
+    func testHouse9() throws {
+        let house = ChalonsTests.houseCusps.ninth
+        XCTAssert(house.sign == Zodiac.pisces)
+        XCTAssert(8 == Int32(house.degree))
+        XCTAssert(57 == Int32(house.minute))
+        XCTAssert(0 == Int32(house.second))
+    }
+
+    func testHouse10() throws {
+        let house = ChalonsTests.houseCusps.midHeaven
+        XCTAssert(house.sign == Zodiac.aries)
+        XCTAssert(8 == Int32(house.degree))
+        XCTAssert(1 == Int32(house.minute))
+        XCTAssert(32 == Int32(house.second))
+    }
+
+    func testHouse11() throws {
+        let house = ChalonsTests.houseCusps.eleventh
+        XCTAssert(house.sign == Zodiac.taurus)
+        XCTAssert(16 == Int32(house.degree))
+        XCTAssert(36 == Int32(house.minute))
+        XCTAssert(24 == Int32(house.second))
+    }
+
+    func testHouse12() throws {
+        let house = ChalonsTests.houseCusps.twelfth
+        XCTAssert(house.sign == Zodiac.gemini)
+        XCTAssert(27 == Int32(house.degree))
+        XCTAssert(29 == Int32(house.minute))
+        XCTAssert(38 == Int32(house.second))
+    }
 }
