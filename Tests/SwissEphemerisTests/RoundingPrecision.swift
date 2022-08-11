209bc3ef13ebee39da77c151494e0cb0effbab49
Sam Krishna <sam.krishna@gmail.com>
2022-08-10 18:20:56 -0700

    Add RoundingPrecision function to SwissEphemeris

---
+++ Tests/SwissEphemerisTests/RoundingPrecision.swift
@@ -0,0 +1,28 @@
+//
+//  RoundingPrecision.swift
+//  
+//
+//  Created by Sam Krishna on 8/10/22.
+//
+
+import Foundation
+
+public enum RoundingPrecision {
+    case ones
+    case tenths
+    case hundredths
+    case thousandths
+}
+
+public func preciseRound(_ value: Double, precision: RoundingPrecision = .ones) -> Double {
+    switch precision {
+    case .ones:
+        return round(value)
+    case .tenths:
+        return round(value * 10) / 10.0
+    case .hundredths:
+        return round(value * 100) / 100.0
+    case .thousandths:
+        return round(value * 1000) / 1000.0
+    }
+}
