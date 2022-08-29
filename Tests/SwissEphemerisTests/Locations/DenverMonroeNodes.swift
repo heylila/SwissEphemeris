4aaf315f1390360d3d2b8dd52075cb57e1ba9454
Sam Krishna <sam.krishna@gmail.com>
2022-08-29 12:51:55 -0700

    Fill out test case for LAD 351's Monroe, LA user

--- Tests/SwissEphemerisTests/Locations/DenverMonroeNodes.swift
+++ Tests/SwissEphemerisTests/Locations/DenverMonroeNodes.swift
@@ -6,30 +6,60 @@
 //
 
 import XCTest
+@testable import SwissEphemeris
 
 class DenverMonroeNodes: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        JPLFileManager.setEphemerisPath()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    static var birthDate: Date {
+        let dob = "1985-07-03 12:57:00 -0500"
+        let dobDate = Date(fromString: dob, format: .cocoaDateTime)!
+        return dobDate
+    }
+
+    static var chart: HouseCusps {
+        let lat = 32.5093109
+        let long = -92.1193012
+        return HouseCusps(date: birthDate, latitude: lat, longitude: long, houseSystem: .placidus)
+    }
+
+    static var planets: [String : Coordinate<Planet> ] {
+        return [
+            Planet.sun.formatted : Coordinate(body: .sun, date: birthDate),
+            Planet.moon.formatted : Coordinate(body: .moon, date: birthDate),
+            Planet.mercury.formatted : Coordinate(body: .mercury, date: birthDate),
+            Planet.venus.formatted : Coordinate(body: .venus, date: birthDate),
+            Planet.mars.formatted : Coordinate(body: .mars, date: birthDate),
+            Planet.jupiter.formatted : Coordinate(body: .jupiter, date: birthDate),
+            Planet.saturn.formatted : Coordinate(body: .saturn, date: birthDate),
+            Planet.uranus.formatted : Coordinate(body: .uranus, date: birthDate),
+            Planet.neptune.formatted : Coordinate(body: .neptune, date: birthDate),
+            Planet.pluto.formatted : Coordinate(body: .pluto, date: birthDate)
+        ]
+    }
+
+    static var nodes: [String : Coordinate<LunarNode> ] {
+        return [
+            "North Node" : Coordinate(body: LunarNode.meanNode, date: birthDate),
+            "South Node" : Coordinate(body: LunarNode.meanSouthNode, date: birthDate)
+        ]
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+        let chart = DenverMonroeNodes.chart
+        let nodes = DenverMonroeNodes.nodes
+        let nnode = nodes["North Node"]
+        let snode = nodes["South Node"]
+        let nnodeCusp = chart.cuspForLongitude(nnode!.longitude)
+        let snodeCusp = chart.cuspForLongitude(snode!.longitude)
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        XCTAssert(nnodeCusp!.name == "eighth")
+        XCTAssert(snodeCusp!.name == "second")
+        XCTAssert(nnodeCusp!.number == 8)
+        XCTAssert(snodeCusp!.number == 2)
     }
 
 }
