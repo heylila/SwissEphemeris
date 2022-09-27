3c836af18a0865a85163f0e440900eabe83e17d7
Sam Krishna <sam.krishna@gmail.com>
2022-09-26 20:26:37 -0700

    Refactored code to be more generic and transportable for retrograde discovery

--- Tests/SwissEphemerisTests/Retrograde2022/JupiterRetrograde.swift
+++ Tests/SwissEphemerisTests/Retrograde2022/JupiterRetrograde.swift
@@ -14,128 +14,78 @@ final class JupiterRetrograde: XCTestCase {
         JPLFileManager.setEphemerisPath()
     }
 
-    // 2022-05-10 to 2022-10-27
     func testJupiterRetrograde() throws {
         // 6 months to check in either direction
         let now = Date(fromString: "2022-09-25 19:25:00 -0700")!
         var past = now.offset(.month, value: -6)!
         var future = now.offset(.month, value: 6)!
-        let body = Planet.jupiter
-        let months = BodiesRequest(body: body).fetch(start: past, end: future, interval: TimeSlice.month.slice)
-        let monthOffsets = Array(months.dropFirst()) + [months.first!]
 
         func testNextTuple<T>(_ now: Coordinate<T>, _ next: Coordinate<T>) -> Bool {
             if now.date > next.date { return false }
-            if next.sign == Zodiac.pisces && now.sign == Zodiac.aries { return true }
-            return next.longitude < now.longitude
+            if now.sign == Zodiac.aries && next.sign == Zodiac.pisces { return true }
+            if now.sign == Zodiac.pisces && next.sign == Zodiac.aries { return false }
+            return now.longitude > next.longitude
         }
 
         func testPastTuple<T>(_ past: Coordinate<T>, _ now: Coordinate<T>) -> Bool {
-            if past.date > now.date { return false }
-            if past.sign == Zodiac.pisces && now.sign == Zodiac.aries { return false }
-            return past.longitude > now.longitude
+            if now.date < past.date { return false }
+            if now.sign == Zodiac.aries && past.sign == Zodiac.pisces { return false }
+            if now.sign == Zodiac.pisces && past.sign == Zodiac.aries { return true }
+            return now.longitude < past.longitude
         }
 
-        let startRxMonth: Coordinate<Planet>? = zip(months, monthOffsets)
-            .first { (now, next) in testNextTuple(now, next) }
-            .map { $0.0 }
+        func findRetrogradeDates<U>(target: U, _ past: Date, _ future: Date, _ time: TimeSlice) -> (past: Date, future: Date)? where U: CelestialBody {
+            let positions = BodiesRequest(body: target).fetch(start: past, end: future, interval: time.slice)
+            let offsets = Array(positions.dropFirst()) + [positions.first!]
 
-        let endRxMonth: Coordinate<Planet>? = Array(zip(months, monthOffsets))
-            .last { (past, now) in testPastTuple(past, now) }
-            .map { $0.1 }
+            let startRx: Coordinate<U>? = zip(positions, offsets)
+                .first { (now, next) in testNextTuple(now, next)}
+                .map { $0.0 }
 
-        guard let startRxMonth = startRxMonth else {
-            XCTFail("We can't find the start of the retrograde... Punting")
-            return
+            let endRx: Coordinate<U>? = Array(zip(positions, offsets))
+                .last { (past, now) in testPastTuple(past, now) }
+                .map { $0.1 }
+
+            guard let startRx = startRx else {
+                XCTFail("We can't find the start of the retrograde... Punting")
+                return nil
+            }
+
+            guard let endRx = endRx else {
+                XCTFail("We can't find the end of the retrograde... Punting")
+                return nil
+            }
+
+            return (startRx.date, endRx.date)
         }
 
-        guard let endRxMonth = endRxMonth else {
-            XCTFail("We can't find the end of the retrograde... Punting")
-            return
+        let timeDict = [
+            TimeSlice.month : (component: Date.DateComponentType.month, value: 0),
+            TimeSlice.day : (component: Date.DateComponentType.day, value: 30),
+            TimeSlice.hour : (component: Date.DateComponentType.hour, value: 24),
+            TimeSlice.minute : (component: Date.DateComponentType.minute, value: 60)
+        ]
+
+        let sliceIndex = 0
+        let slices: [TimeSlice] = [ .month, .day, .hour, .minute ]
+        for i in stride(from: sliceIndex, to: slices.endIndex, by: 1) {
+            let time = slices[i]
+            let offsetTuple = timeDict[time]!
+            past = past.offset(offsetTuple.component, value: (-1 * offsetTuple.value))!
+            future = future.offset(offsetTuple.component, value: offsetTuple.value)!
+            let window = findRetrogradeDates(target: Planet.jupiter, past, future, time)
+            guard let window = window else { break }
+            past = window.past
+            future = window.future
         }
 
-        past = startRxMonth.date.offset(.day, value: -30)!
-        future = endRxMonth.date.offset(.day, value: 30)!
+        // 2022-07-28 to 2022-11-23
+        XCTAssert(past.component(.month)! == 7, "Erroneoes Rx starting month is \(past.component(.month)!)")
+        XCTAssert(past.component(.day)! == 28, "Erroneoes Rx starting day is \(past.component(.day)!)")
+        XCTAssert(past.component(.year)! == 2022)
 
-        let days = BodiesRequest(body: body).fetch(start: past, end: future, interval: TimeSlice.day.slice)
-        let dayOffsets = Array(days.dropFirst()) + [days.first!]
-
-        let startRxDay: Coordinate<Planet>? = zip(days, dayOffsets)
-            .first { (now, next) in testNextTuple(now, next) }
-            .map { $0.0 }
-
-        let endRxDay: Coordinate<Planet>? = Array(zip(days, dayOffsets))
-            .last { (past, now) in testPastTuple(past, now) }
-            .map { $0.1 }
-
-        guard let startRxDay = startRxDay else {
-            XCTFail("We can't find the start of the retrograde... Punting")
-            return
-        }
-
-        guard let endRxDay = endRxDay else {
-            XCTFail("We can't find the end of the retrograde... Punting")
-            return
-        }
-
-        past = startRxDay.date.offset(.hour, value: -24)!
-        future = endRxDay.date.offset(.hour, value: 24)!
-
-        let hours = BodiesRequest(body: body).fetch(start: past, end: future, interval: TimeSlice.hour.slice)
-        let hourOffsets = Array(hours.dropFirst()) + [hours.first!]
-
-        let startRxHour: Coordinate<Planet>? = zip(hours, hourOffsets)
-            .first { (now, next) in testNextTuple(now, next) }
-            .map { $0.0 }
-
-        let endRxHour: Coordinate<Planet>? = Array(zip(hours, hourOffsets))
-            .last { (past, now) in testPastTuple(past, now) }
-            .map { $0.1 }
-
-        guard let startRxHour = startRxHour else {
-            XCTFail("We can't find the start of the retrograde... Punting")
-            return
-        }
-
-        guard let endRxHour = endRxHour else {
-            XCTFail("We can't find the end of the retrograde... Punting")
-            return
-        }
-
-        past = startRxHour.date.offset(.minute, value: -60)!
-        future = endRxHour.date.offset(.minute, value: 60)!
-
-        let minutes = BodiesRequest(body: body).fetch(start: past, end: future, interval: TimeSlice.minute.slice)
-        let minuteOffsets = Array(minutes.dropFirst()) + [minutes.first!]
-
-        let startRxMinute: Coordinate<Planet>? = zip(minutes, minuteOffsets)
-            .first { (now, next) in testNextTuple(now, next) }
-            .map { $0.0 }
-
-        let endRxMinute: Coordinate<Planet>? = Array(zip(minutes, minuteOffsets))
-            .last { (past, now) in testPastTuple(past, now) }
-            .map { $0.1 }
-
-        guard let startRxMinute = startRxMinute else {
-            XCTFail("We can't find the start of the retrograde... Punting")
-            return
-        }
-
-        guard let endRxMinute = endRxMinute else {
-            XCTFail("We can't find the end of the retrograde... Punting")
-            return
-        }
-
-        let minuteRxStart = startRxMinute.date
-        let minuteRxEnd = endRxMinute.date
-
-        // 2022-05-10 to 2022-11-23
-        XCTAssert(minuteRxStart.component(.month)! == 5)
-        XCTAssert(minuteRxStart.component(.day)! == 10)
-        XCTAssert(minuteRxStart.component(.year)! == 2022)
-
-        XCTAssert(minuteRxEnd.component(.month)! == 11)
-        XCTAssert(minuteRxEnd.component(.day)! == 23)
-        XCTAssert(minuteRxEnd.component(.year)! == 2022)
+        XCTAssert(future.component(.month)! == 11, "Erroneoes Rx starting month is \(past.component(.month)!)")
+        XCTAssert(future.component(.day)! == 23, "Erroneoes Rx starting month is \(past.component(.day)!)")
+        XCTAssert(future.component(.year)! == 2022)
     }
 }
