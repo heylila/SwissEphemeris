097fe52a361dbac151c286a29250ea29b6386835
Sam Krishna <sam.krishna@gmail.com>
2022-10-12 19:39:12 -0700

    Add more filtering capabilities to `CelestialObject`

--- Sources/SwissEphemeris/Protocols/CelestialObject.swift
+++ Sources/SwissEphemeris/Protocols/CelestialObject.swift
@@ -7,9 +7,38 @@
 
 import Foundation
 
-public enum CelestialObject {
+public enum CelestialObject: Codable {
     case asteroid(Asteroid)
     case lunarNode(LunarNode)
     case planet(Planet)
     case fixedStar(FixedStar)
+}
+
+extension CelestialObject {
+    var asteroid: Asteroid? {
+        switch self {
+        case let .asteroid(asteroid):
+            return asteroid
+        default:
+            return nil
+        }
+    }
+
+    var planet: Planet? {
+        switch self {
+        case let .planet(planet):
+            return planet
+        default:
+            return nil
+        }
+    }
+
+    var lunarNode: LunarNode? {
+        switch self {
+        case let .lunarNode(lunarNode):
+            return lunarNode
+        default:
+            return nil
+        }
+    }
 }
