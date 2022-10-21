bf8bc03726ab8ea3eb3174c11010a36a68c6cd6a
Sam Krishna <sam.krishna@gmail.com>
2022-10-21 12:41:34 -0700

    Make CelestialBody conform to Hashable

--- Sources/SwissEphemeris/Protocols/CelestialBody.swift
+++ Sources/SwissEphemeris/Protocols/CelestialBody.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 /// Models a planet, moon, asteroid, star or celestial body both real or imaginary.
-public protocol CelestialBody: CaseIterable, Codable, Equatable {
+public protocol CelestialBody: CaseIterable, Codable, Equatable, Hashable {
 	associatedtype DateType
 	/// The IPL body number.
 	var value: DateType { get }
