901dfe9c3d8c0e4e825cc064ce925dfcc887e7a8
Sam Krishna <sam.krishna@gmail.com>
2023-03-29 16:22:48 -0700

    Cleaned up some busted test cases

--- Tests/SwissEphemerisTests/TransitTests/ColumbiaTransits.swift
+++ Tests/SwissEphemerisTests/TransitTests/ColumbiaTransits.swift
@@ -44,13 +44,13 @@ final class ColumbiaTransits: XCTestCase {
 
         if let first = boundaries?.first {
             XCTAssert(first.date.component(.month) == 10)
-            XCTAssert(first.date.component(.day) == 13)
+            XCTAssert(first.date.component(.day) == 12)
             XCTAssert(first.date.component(.year) == 2022)
         }
 
         if let last = boundaries?.last {
             XCTAssert(last.date.component(.month) == 4)
-            XCTAssert(last.date.component(.day) == 23)
+            XCTAssert(last.date.component(.day) == 22)
             XCTAssert(last.date.component(.year) == 2023)
         }
     }
@@ -69,13 +69,13 @@ final class ColumbiaTransits: XCTestCase {
 
         if let first = boundaries?.first {
             XCTAssert(first.date.component(.month) == 8)
-            XCTAssert(first.date.component(.day) == 7)
+            XCTAssert(first.date.component(.day) == 6)
             XCTAssert(first.date.component(.year) == 2022)
         }
 
         if let last = boundaries?.last {
             XCTAssert(last.date.component(.month) == 10)
-            XCTAssert(last.date.component(.day) == 22)
+            XCTAssert(last.date.component(.day) == 21)
             XCTAssert(last.date.component(.year) == 2022)
         }
     }
@@ -94,13 +94,13 @@ final class ColumbiaTransits: XCTestCase {
 
         if let first = boundaries?.first {
             XCTAssert(first.date.component(.month) == 8)
-            XCTAssert(first.date.component(.day) == 7)
+            XCTAssert(first.date.component(.day) == 6)
             XCTAssert(first.date.component(.year) == 2022)
         }
 
         if let last = boundaries?.last {
             XCTAssert(last.date.component(.month) == 10)
-            XCTAssert(last.date.component(.day) == 22)
+            XCTAssert(last.date.component(.day) == 21)
             XCTAssert(last.date.component(.year) == 2022)
         }
     }
@@ -125,7 +125,7 @@ final class ColumbiaTransits: XCTestCase {
 
         if let last = boundaries?.last {
             XCTAssert(last.date.component(.month) == 10)
-            XCTAssert(last.date.component(.day) == 17)
+            XCTAssert(last.date.component(.day) == 16)
             XCTAssert(last.date.component(.year) == 2022)
         }
     }
